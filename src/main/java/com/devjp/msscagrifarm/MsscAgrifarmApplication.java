57b74f078dc241746405f2a31da2ab8a9c6703fc
Mani OP360 <mani@office-partners360.com>
2019-10-23 17:42:41 +0530

    Initial Submit

---
+++ src/main/java/com/devjp/msscagrifarm/MsscAgrifarmApplication.java
@@ -0,0 +1,13 @@
+package com.devjp.msscagrifarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscAgrifarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscAgrifarmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devjp/msscagrifarm/MsscAgrifarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjp.msscagrifarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscAgrifarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
