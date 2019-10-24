ae5bf8afb3fe5da81d1fda204e97134439c66d95
Jophin P <jophinac@gmail.com>
2019-10-24 15:23:03 +0530

    Added HTTP Operations on Customer

--- src/main/java/com/devjp/msscagrifarm/services/CustomerServiceImpl.java
+++ src/main/java/com/devjp/msscagrifarm/services/CustomerServiceImpl.java
@@ -1,10 +1,12 @@
 package com.devjp.msscagrifarm.services;
 
 import com.devjp.msscagrifarm.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService {
     @Override
@@ -14,5 +16,22 @@ public class CustomerServiceImpl implements CustomerService {
                 .name("Jophin P")
                 .phone("7745599019")
                 .build();
+    }
+
+    @Override
+    public CustomerDto saveCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+
+    }
+
+    @Override
+    public void deleteById(UUID customerId) {
+        log.debug("Deleting Customer");
     }
 }
--- src/main/java/com/devjp/msscagrifarm/web/controller/CustomerController.java
+++ src/main/java/com/devjp/msscagrifarm/web/controller/CustomerController.java
@@ -2,12 +2,10 @@ package com.devjp.msscagrifarm.web.controller;
 
 import com.devjp.msscagrifarm.services.CustomerService;
 import com.devjp.msscagrifarm.web.model.CustomerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -23,5 +21,26 @@ public class CustomerController {
     @GetMapping({"/{customerId}"})
     public ResponseEntity<CustomerDto> getVegetable(@PathVariable("customerId") UUID customerId){
         return new ResponseEntity<>(customerService.getCustomerId(customerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity handlePost(@RequestBody CustomerDto customerDto)
+    {
+        CustomerDto savedDto = customerService.saveCustomer(customerDto);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Location", "/api/v1/customer/"+savedDto.getId());
+        return new ResponseEntity(httpHeaders, HttpStatus.OK);
+    }
+
+    @PutMapping({"/{customerId}"})
+    public ResponseEntity handlePut(@RequestBody CustomerDto customerDto, @PathVariable UUID customerId){
+        customerService.updateCustomer(customerId,customerDto);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping ({"/{customerId}"})
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCustomer(@PathVariable UUID customerId){
+        customerService.deleteById(customerId);
     }
 }
